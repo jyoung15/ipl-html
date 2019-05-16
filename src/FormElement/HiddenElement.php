f70a773e31164c59dcba352551dab8ac6a7a4e1d
Thomas Gelf <thomas@gelf.net>
2019-05-16 14:21:43 +0200

    WIP: HiddenElement: allow to lock a specific value
    Still unsure whether this would be helpful

--- src/FormElement/HiddenElement.php
+++ src/FormElement/HiddenElement.php
@@ -5,4 +5,37 @@ namespace ipl\Html\FormElement;
 class HiddenElement extends InputElement
 {
     protected $type = 'hidden';
+
+    protected $protected = false;
+
+    protected function registerCallbacks()
+    {
+        parent::registerCallbacks();
+        $this->getAttributes()->registerAttributeCallback(
+            'protected',
+            null,
+            [$this, 'setProtected']
+        );
+    }
+
+    public function setValue($value)
+    {
+        if ($this->value === null || ! $this->protected) {
+            parent::setValue($value);
+        }
+
+        return $this;
+    }
+
+    public function isProtected()
+    {
+        return $this->protected;
+    }
+
+    public function setProtected($protected = true)
+    {
+        $this->protected = (bool) $protected;
+
+        return $this;
+    }
 }
